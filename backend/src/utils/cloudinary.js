96aeb06f8f409f610e6a1e052f942b9f8d3427be
sailendrachettri <sailendra9083@gmail.com>
2024-05-12 22:07:55 +0530

    cloudinary code qritten

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try{
+        // file not found
+        if(!localFilePath) return null;
+
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+
+        // file has been uploaded successfully
+        console.log("file has been uploaded successfully on cloudinary");
+        console.log("uploaded url: ", response.url);
+
+        return response;
+
+    } catch(err){
+        // if not uploaded on cloudinary the remove the locally saved file
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
